14d6ea3b8be4ac150d1b0b30be5c73959a233b0f
Papkidon <81909489+Papkidon@users.noreply.github.com>
2023-03-04 20:42:57 +0100

    refactor: remove unnecessary imports in modules

--- services/nest/src/modules/view/view.module.ts
+++ services/nest/src/modules/view/view.module.ts
@@ -1,20 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ViewController } from './view.controller';
-import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [
-    LoggerModule.forRoot({
-      pinoHttp: {
-        transport: {
-          target: 'pino-pretty',
-          options: {
-            singleLine: true,
-          },
-        },
-      },
-    }),
-  ],
   controllers: [ViewController],
 })
 export class ViewModule {}
