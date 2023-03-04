222e4f6d997a18f1f0af6cffc708ddf3271e90e5
Papkidon <81909489+Papkidon@users.noreply.github.com>
2023-03-04 19:04:00 +0100

    feat(django): add aggregating endpoint

--- services/django/dashboards/admin_app/views.py
+++ services/django/dashboards/admin_app/views.py
@@ -1,7 +1,41 @@
+import json
+from collections import Counter
+
+import pandas as pd
+import requests
 from django.http import HttpResponse
 
+from .utils.utils import flatten_list
+
+
+def get_letter_count(data: pd.Series) -> int:
+    flat_data = flatten_list(data)
+    words_list = [letter.split() for letter in flat_data]
+    letters_list = flatten_list(words_list)
+    return dict(Counter(letters_list))
 
 # To be used
 # https://django-plotly-dash.readthedocs.io/en/latest/introduction.html
+
+
 def index(request):
     return HttpResponse('dashboard here')
+
+
+def aggregate(request):
+    admin_data = requests.get(
+        url='http://nest:3000/api/v1/admin', timeout=3)
+
+    data = pd.DataFrame(admin_data.json())
+
+    letters = get_letter_count(data)
+
+    exceptions = [exc for exc in admin_data if isinstance(exc, Exception)]
+    if exceptions:
+        raise ExceptionGroup("Couldn't retrieve admin data: ", exceptions)
+
+    return HttpResponse(json.dumps(letters), content_type="application/json")
+
+
+def visualize(request):
+    raise NotImplementedError("Visualize method is not implemented yet.")
--- services/django/dashboards/dashboards/urls.py
+++ services/django/dashboards/dashboards/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('dashboard/', include('admin_app.urls')),
     path('admin/', admin.site.urls),
+    path('aggregate/', include('admin_app.urls')),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ]
