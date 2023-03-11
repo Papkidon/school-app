ffe9bb2433bd7d17444dc26b35b1277e05555d1b
Papkidon <pawelkopaniecki40@gmail.com>
2023-03-11 13:35:28 +0100

    fix: strict validation in zod schemas

--- services/nest/src/models/admin/admin.types.ts
+++ services/nest/src/models/admin/admin.types.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
-export const adminCreateSchema = z.object({
-  data: z.object({
-    username: z.string().min(2).max(20),
-    password: z.string().min(8).max(20),
-  }),
-});
+export const adminCreateSchema = z
+  .object({
+    data: z.object({
+      username: z.string().min(2).max(20),
+      password: z.string().min(8).max(20),
+    }),
+  })
+  .strict();
 
 export const adminUpdateSchema = adminCreateSchema.deepPartial();
 
